38472b1c4c8896f5f06aef428a4df51d12423178
Alisson Pintor <alissonpintor@gmail.com>
2017-01-17 17:31:53 -0300

    Adicionado Conexao com o DB2 no SQLAlchemy

--- app/models/tables.py
+++ app/models/tables.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('sqlite:///database.db', echo=True)
+#engine = create_engine('sqlite:///database.db', echo=True)
+engine = create_engine("db2+ibm_db://relatorio:relatorio@192.168.104.3:50000/STOKY")
 #from app import Base
 
 class User(Base):
@@ -33,6 +34,23 @@ class Produtos(Base):
 		self.descricao = descricao
 		self.fabricante = fabricante
 		self.saldo = saldo
+
+	def __repr__(self):
+		return 'ID: %d - DESCRICAO: %s - MARCA: %s - SALDO: %f' % (self.id, self.descricao, self.fabricante, self.saldo)
+
+class ProdutosCiss(Base):
+	__tablename__ = 'dba.produtos_view'
+	idsubproduto = Column(Integer, primary_key=True)
+	descricaoproduto = Column(String(45), nullable=False)
+	fabricante = Column(String(20), nullable=False)
+
+	def __init__(self, descricao, fabricante, saldo):
+		self.descricao = descricao
+		self.fabricante = fabricante
+		self.saldo = saldo
+
+	def __repr__(self):
+		return 'ID: %d - DESCRICAO: %s - MARCA: %s' % (self.id, self.descricao, self.fabricante)
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
