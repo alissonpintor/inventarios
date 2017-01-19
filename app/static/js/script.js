167bb853ef35fa9e00343cb35199566c383590b6
Alisson Pintor <alissonpintor@gmail.com>
2017-01-19 15:34:30 -0300

    Alteracoes feitas no .gitignore

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,16 +1,7 @@
 $(function() {
-    var saldo = $('.saldo');
 
-    $('#hide-zero').bind('click', function(){
-      if($('#hide-zero:checked')){
+    $('#hide-zero').bind('click', function() {
 
-        if($(saldo).html() == 0.0 || $(saldo).html() == 0){
-            console.log('teste');
-            $(saldo).closest('tr').css('display', 'none');
-
-        };
-
-      };
     });
 
     $("#hide").bind("click", function() {
@@ -25,30 +16,29 @@ $(function() {
         }
     });
 
-    $('.box-filter').bind('blur', function(){
-    	//Acoes...
-      $('.box-filter').toggle('fast');
+    $('.box-filter').bind('blur', function() {
+        //Acoes...
+        $('.box-filter').toggle('fast');
     });
 
-    $('.button-filter').bind('click', function(){
-      var ele = $('.button-filter');
+    $('.button-filter').bind('click', function() {
+        var ele = $('.button-filter');
 
-      if($(ele).hasClass('rotatereverse')){
-        $(ele).removeClass('rotatereverse');
-        $(ele).addClass('rotate');
-      }
-      else if ($(ele).hasClass('rotate')){
-        $(ele).removeClass('rotate');
-        $(ele).addClass('rotatereverse');
-      }
-
-      $('.box-filter').toggle('fast');
-      if($('.box-filter').css('display') == 'block'){
-        //$('.box-filter').toggle('fast');
-        //$('#text').focus();
-        if($('.box-filter').is(':focus')){
-            console.log('bbb');
+        if ($(ele).hasClass('rotatereverse')) {
+            $(ele).removeClass('rotatereverse');
+            $(ele).addClass('rotate');
+        } else if ($(ele).hasClass('rotate')) {
+            $(ele).removeClass('rotate');
+            $(ele).addClass('rotatereverse');
         }
-      }
+
+        $('.box-filter').toggle('fast');
+        if ($('.box-filter').css('display') == 'block') {
+            //$('.box-filter').toggle('fast');
+            //$('#text').focus();
+            if ($('.box-filter').is(':focus')) {
+                console.log('bbb');
+            }
+        }
     });
 });
